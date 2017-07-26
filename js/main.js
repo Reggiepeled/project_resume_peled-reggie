639d12f8fec96e436634898f9d72d4898bded2c8
reggie peled <revital.peled@gmail.com>
2017-07-26 19:56:40 -0400

    add tootipster plugin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function () {
+
+    //activate the snow effect plugin
     $.fn.snow();
+
+    //activate the tooltipster Plugin on work, education and projects
+    $('.tooltip').tooltipster();
+
+    //activate the tooltipster Plugin on pottery section, use image
+    $('.potterytooltip').tooltipster({
+        content: $('<span><img class="tipimg" src="img/pottery.jpg" /></span>')
+    });
+
+    //activate the tooltipster Plugin on wood section, use image
+    $('.woodtooltip').tooltipster({
+        content: $('<span><img class="tipimg" src="img/bench.jpg" /></span>')
+    });
+
+    //activate the tooltipster Plugin on crochet section, use image
+    $('.crochettooltip').tooltipster({
+        content: $('<span><img class="tipimg" src="img/scarf.jpg" /></span>')
+    });
+
+    //activate the tooltipster Plugin on doodle section, use image
+    $('.doodletooltip').tooltipster({
+        content: $('<span><img class="tipimg" src="img/butterfly.jpg" /></span>')
+    });
+
 });
